a555acbd616aecd92196d261854bdf3d4c224cee
Palak Chauhan <palaksingh.chauhan08@gmail.com>
2022-12-05 19:43:21 +0530

    Add files via upload

---
+++ Pages/Ingredients.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+st.sidebar.markdown('')
+
+# st.markdown("<h1 style='text-align: center; color: red;'>Some title</h1>", unsafe_allow_html=True)
